05358f387f682bdbc5efd7942cc1acf2fcf10953
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-05-17 19:36:44 +0400

    sample with different runtime errors

--- jasmine/evanhahn/spec/personSpec.js
+++ jasmine/evanhahn/spec/personSpec.js
@@ -1,17 +1,22 @@
-describe("Person", function() {
-    it("calls the sayHello() function", function() {
-        var fakePerson = new Person();
-        spyOn(fakePerson, "sayHello");
-        fakePerson.helloSomeone("world");
-        expect(fakePerson.sayHello).toHaveBeenCalled();
-    });
+describe("Person", function () {
+  it("calls the sayHello() function", function () {
+    var fakePerson = new Person();
+    spyOn(fakePerson, "sayHello");
+    fakePerson.helloSomeone("world");
+    expect(fakePerson.sayHello).toHaveBeenCalled();
+  });
 
-    describe("Person", function() {
-        it("greets the world", function() {
-            var fakePerson = new Person();
-            spyOn(fakePerson, "helloSomeone");
-            fakePerson.helloSomeone("world");
-            expect(fakePerson.helloSomeone).toHaveBeenCalledWith("world");
-        });
+  describe("WorldPerson", function () {
+    it("greets the world", function () {
+      var fakePerson = new Person();
+      spyOn(fakePerson, "helloSomeone");
+      fakePerson.helloSomeone("world");
+      expect(fakePerson.helloSomeone).toHaveBeenCalledWith("world");
     });
+    it("calls undefined method", function () {
+      undefiedMethod();
+    });
+  });
 });
+
+anotherUndefiedMethod();
