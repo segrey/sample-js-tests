90d1b114105d34760963af0ecf65a1015bbbfb2d
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-06-03 12:42:39 +0400

    tabs -> spaces

--- jasmine/jasmin_tutsplus/spec/MyObjectSpec.js
+++ jasmine/jasmin_tutsplus/spec/MyObjectSpec.js
@@ -8,11 +8,11 @@ describe("MyObject", function () {
         obj.setState("dirty");
         console.log(obj);
         expect(obj.getState()).toEqual("dirty");
-    })
+    });
     it("adds states", function () {
         obj.addState("packaged");
         expect(obj.getState()).toEqual(["clean", "packaged"]);
-    })
+    });
 });
 
 describe("MyObject", function () {
@@ -24,30 +24,30 @@ describe("MyObject", function () {
     it("changes state", function () {
         obj.setState("dirty");
         expect(obj.getState()).toEqual("dirty");
-    })
+    });
     it("adds states", function () {
         obj.addState("packaged");
         expect(obj.getState()).toEqual(["clean", "packaged"]);
-    })
+    });
 });
-	
+
 describe("test", function () {
-    beforeEach(function () {
-		this.addMatchers({
-			toBeBetween: function (rangeFloor, rangeCeiling) {
-				if (rangeFloor > rangeCeiling) {
-					var temp = rangeFloor;
-					rangeFloor = rangeCeiling;
-					rangeCeiling = temp;
-				}
-				return this.actual > rangeFloor && this.actual < rangeCeiling;
-			}
-		});
-	});
-	it("works", function () {
-	    expect(10).toBeBetween(5, 30);
-	});
-	it("works too", function () {
-	    expect(100).toBeBetween(500, 30);
-	});
+  beforeEach(function () {
+    this.addMatchers({
+      toBeBetween: function (rangeFloor, rangeCeiling) {
+        if (rangeFloor > rangeCeiling) {
+          var temp = rangeFloor;
+          rangeFloor = rangeCeiling;
+          rangeCeiling = temp;
+        }
+        return this.actual > rangeFloor && this.actual < rangeCeiling;
+      }
+    });
+  });
+  it("works", function () {
+    expect(10).toBeBetween(5, 30);
+  });
+  it("works too", function () {
+    expect(100).toBeBetween(500, 30);
+  });
 });
