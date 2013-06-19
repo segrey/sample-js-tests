0ded31930d05383853a61d59788a881303ead34d
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-06-19 19:45:53 +0400

    configs updated to be compatible with karma@0.9.3

--- jasmine/evanhahn/jasmine.karma.conf.js
+++ jasmine/evanhahn/jasmine.karma.conf.js
@@ -1,60 +1,62 @@
 // Karma configuration
 // Generated on Thu May 16 2013 01:34:14 GMT+0400 (MSK)
 
+module.exports = function (karma) {
+  karma.configure({
 
 // base path, that will be used to resolve files and exclude
-basePath = '';
+    basePath: '',
 
 
 // frameworks to use
-frameworks = ['jasmine'];
+    frameworks: ['jasmine'],
 
 
 // list of files / patterns to load in the browser
-files = [
-  'src/**/*.js',
-  'spec/**/*Spec.js'
-];
+    files: [
+      'src/**/*.js',
+      'spec/**/*Spec.js'
+    ],
 
 
 // list of files to exclude
-exclude = [
-  
-];
+    exclude: [
+
+    ],
 
 
 // test results reporter to use
 // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-reporters = ['progress', 'coverage'];
+    reporters: ['progress', 'coverage'],
 
-preprocessors = {
-    '**/src/**/*.js': 'coverage'
-};
+    preprocessors: {
+      '**/src/**/*.js': 'coverage'
+    },
 
-coverageReporter = {
-    type : 'lcov',
-    dir : '/home/segrey/WebstormProjects/sample-js-tests/jasmine/evanhahn/coverage'
-};
+    coverageReporter: {
+      type: 'lcov',
+      dir: '/home/segrey/WebstormProjects/sample-js-tests/jasmine/evanhahn/coverage'
+    },
 
 // web server port
-port = 9876;
+    port: 9876,
 
 
 // cli runner port
-runnerPort = 9100;
+    runnerPort: 9100,
 
 
 // enable / disable colors in the output (reporters and logs)
-colors = true;
+    colors: true,
 
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_DEBUG;
+    logLevel: karma.LOG_DEBUG,
 
 
 // enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    autoWatch: true,
 
 
 // Start these browsers, currently available:
@@ -65,21 +67,23 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = [];
+    browsers: [],
 
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    captureTimeout: 60000,
 
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
-singleRun = false;
+    singleRun: false,
 
 
 // plugins to load
-plugins = [
-  'karma-jasmine'
-  , 'karma-chrome-launcher'
-  , 'karma-coverage'
-];
+    plugins: [
+      'karma-jasmine'
+      , 'karma-chrome-launcher'
+      , 'karma-coverage'
+    ]
+  });
+};
--- qunit/pretty-date/prettydate.karma.conf.js
+++ qunit/pretty-date/prettydate.karma.conf.js
@@ -1,77 +1,77 @@
 // Karma configuration
-// Generated on Fri May 31 2013 12:08:11 GMT+0400 (MSK)
+// Generated on Wed Jun 19 2013 15:00:04 GMT+0400 (MSK)
+
+module.exports = function(karma) {
+  karma.configure({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['qunit'],
 
 
-// frameworks to use
-frameworks = ['qunit'];
+    // list of files / patterns to load in the browser
+    files: [
+      'src/**/*.js',
+      'tests/**/*.js',
+      '../basic/**/*.js'
+    ],
 
 
-// list of files / patterns to load in the browser
-files = [
-  'src/**/*.js',
-  'tests/**/*.js',
-  '../basic/**/*.js'
-];
+    // list of files to exclude
+    exclude: [
+
+    ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-reporters = ['progress'];
+    // web server port
+    port: 9876,
 
 
-// web server port
-port = 9876;
+    // cli runner port
+    runnerPort: 9100,
 
 
-// cli runner port
-runnerPort = 9100;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // level of logging
+    // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
+    logLevel: karma.LOG_INFO,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: [],
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = [];
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
-
-
-// plugins to load
-plugins = [
-  'karma-qunit',
-  'karma-chrome-launcher'
-];
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+    plugins: [
+      'karma-qunit'
+    ]
+  });
+};
