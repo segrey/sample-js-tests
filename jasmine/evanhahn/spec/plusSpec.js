f8852cff4c3aa272b8b3d568b5bbe45bf476ff8d
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-07-23 14:03:54 +0400

    plusSpec.js added

---
+++ jasmine/evanhahn/spec/plusSpec.js
@@ -0,0 +1,11 @@
+describe("plus operation", function() {
+
+  it("sums numbers", function () {
+    expect(1 + 1).toBe(2);
+  });
+
+  xit("sums incorrectly sometimes", function () {
+    expect(2 + 2).toBe(5);
+  });
+
+});
