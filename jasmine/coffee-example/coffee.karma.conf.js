c0c39d988f9e0a689111a579775faa1a2b0fbdf2
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-10-16 13:02:29 +0400

    coffee sample

---
+++ jasmine/coffee-example/coffee.karma.conf.js
@@ -0,0 +1,89 @@
+// Karma configuration
+// Generated on Thu May 16 2013 01:34:14 GMT+0400 (MSK)
+
+module.exports = function (config) {
+  config.set({
+
+// base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+// frameworks to use
+    frameworks: ['jasmine'],
+
+
+// list of files / patterns to load in the browser
+    files: [
+      'spec/**/*.coffee'
+    ],
+
+
+// list of files to exclude
+    exclude: [
+
+    ],
+
+
+// test results reporter to use
+// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+    preprocessors: {
+      '**/*.coffee': 'coffee'
+    },
+
+    coffeePreprocessor: {
+      // options passed to the coffee compiler
+      options: {
+        bare: true,
+        sourceMap: true
+      },
+      transformPath: function(path) {
+        return path.replace(/\.js$/, '.coffee');
+      }
+    },
+
+// web server port
+    port: 9876,
+
+
+// enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+
+// enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
+//    browsers: ['Chrome', 'Firefox'],
+
+
+// If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+// Continuous Integration mode
+// if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+// plugins to load
+    plugins: [
+      'karma-jasmine'
+      , 'karma-chrome-launcher'
+      , 'karma-coffee-preprocessor'
+    ]
+  });
+};
---
+++ jasmine/coffee-example/spec/helloWorldSpec.coffee
@@ -0,0 +1,3 @@
+describe 'World', ->
+  it 'is divisible by 2', ->
+    expect(2 % 2).toBe(0)
