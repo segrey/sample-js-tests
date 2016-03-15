c6e0bfc1a208cf7128b9c9bcc42f1dde8623fdfd
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2016-03-15 13:38:44 +0300

    temporary test data

---
+++ jasmine/WEB-20774/testreproduce.js
@@ -0,0 +1,32 @@
+'use strict';
+var collabo;
+(function (collabo) {
+    var controllers;
+    (function (controllers) {
+        describe('コントローラのテスト', function () {
+            //前準備と依存性の注入 (トップレベル)
+            // beforeEach(inject(function ($injector, $rootScope, $controller) {
+            // }));
+            /**
+             *  AdminFormController の各種テスト
+             *
+             */
+            describe('文字化け再現の為のテスト', function () {
+                //コントローラの生成と依存性注入
+                // beforeEach(inject(function ($injector, $rootScope, $controller) {
+                // }));
+                it('インスタンス生成と初期値確認', function () {
+                });
+                it('テナント情報取得のテスト', function () {
+                });
+                it('テナント情報挿入のテスト', function () {
+                });
+                it('テナント情報変更のテスト', function () {
+                });
+                it('テナント情報変更のテスト', function () {
+                });
+            });
+        });
+    })(controllers = collabo.controllers || (collabo.controllers = {}));
+})(collabo || (collabo = {}));
+//# sourceMappingURL=testreproduce.js.map
